c1d31dde1d753e2fff32c48b721da79b404cda0e
Sakshi Sukhnani <sukhnanisakshi@gmail.com>
2025-12-07 12:35:41 +0000

    The pictures arent loading

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
